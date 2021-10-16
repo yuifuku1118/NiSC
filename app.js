60631cbd84301d8a7129fabd6ee20ecb44e06801
Yuichiro Fukushima <e0407814@u.nus.edu>
2021-10-16 18:03:38 +0800

    adding

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const url = require('url');
 
 const indexPage = fs.readFileSync('./html/index.html', 'UTF-8');
 const articles = fs.readFileSync('./html/pages/articles.html', 'UTF-8');
+const styleCss = fs.readFileSync('./html/css/style.css', 'UTF-8');
+const scriptjs = fs.readFileSync('./html/js/script.js', 'UTF-8');
 
 
 const hostname = '127.0.0.1';
@@ -28,6 +30,19 @@ function RouteSetting(req, res) {
       case '/kanumaOne.html':
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(articles);
+        res.end();
+        break;
+
+    case 'css':
+    case '/css/style.css':
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(styleCss);
+        res.end();
+        break;
+
+    case '/js/script.js':
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(scriptjs);
         res.end();
         break;
 
